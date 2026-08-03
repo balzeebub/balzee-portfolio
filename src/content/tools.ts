775b6b3cf037197f260ebc741a359edee72da73d
balzeebub <jmbalderama019@gmail.com>
2026-08-03 09:48:08 +0800

    Update tools.ts

--- src/content/tools.ts
+++ src/content/tools.ts
@@ -115,8 +115,8 @@ export const clients: Client[] = [
     name: "Everyday Order",
     logo: {
       src: "/logos/everydayorder.png",
-      width: 512,
-      height: 128,
+      width: 1200,
+      height: 300,
     },
   },
 ];
